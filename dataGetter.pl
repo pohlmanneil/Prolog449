56b3d6616d2118402beba527a132ab230255925e
ErickPower <35816207+ErickPower@users.noreply.github.com>
2018-04-11 14:44:19 -0600

    Add files via upload

--- dataGetter.pl
+++ dataGetter.pl
@@ -1,39 +1,40 @@
 %%Data is either the wanted data, or an error code
 %%RestOfText is either the text, with the data dropped, or the error message corresponding to Data.
 
-getData([H|Text],Data,RestOfText):- H == "forced partial assignment:",
+getData([H|Text],Data,RestOfText,Err):- H == "forced partial assignment:",
     nth(Next,Text,"forbidden machine:"),
     Len is Next-1,
     split(Len,Text,RawDat,RestOfText),
     twoMachTaskToInts(RawDat,Data,Err).
 
-getData([H|Text],Data,RestOfText):- H == "forbidden machine:",
+getData([H|Text],Data,RestOfText,Err):- H == "forbidden machine:",
     nth(Next,Text,"too-near tasks:"),
     Len is Next-1,
     split(Len,Text,RawDat,RestOfText),
     twoMachTaskToInts(RawDat,Data,Err).
 
-getData([H|Text],Data,RestOfText):- H == "too-near tasks:",
+getData([H|Text],Data,RestOfText,Err):- H == "too-near tasks:",
     nth(Next,Text,"machine penalties:"),
     Len is Next-1,
     split(Len,Text,RawDat,RestOfText),
     twoTaskTaskToInts(RawDat,Data,Err).
 
-getData([H|Text],Data,RestOfText):- H == "machine penalties:",
+getData([H|Text],Data,RestOfText,Err):- H == "machine penalties:",
     nth(Next,Text,"too-near penalities"),
     Len is Next-1,
     split(Len,Text,RawDat,RestOfText). %TODO: Add parser for int grid
 
-getData([H|Text],Data,RestOfText):- H == "too-near penalities",
-    thrBrackToInts(Text,MaybeData,ErrFlag),
-    (ErrFlag == -1 *-> (Data = -1, RestOfText = 'GARBAGE', Foo is 1); Foo is 0),
-    (ErrFlag == -2 *-> (RestOfText = 'invalid penalty', Data = -1, Bar is 1); Bar is 0),
-    Tik is (Foo + Bar),
-	(Tik == 0 *-> (Data = MaybeData, RestofText = []); Wee is 0).
+getData([H|Text],Data,RestOfText,Err):- H == "too-near penalities",
+    thrBrackToInts(Text,Data,Err), RestOfText = 'Empty'.
+	
+    %(ErrFlag == -1 *-> (Data = -1, RestOfText = 'GARBAGE', Foo is 1); Foo is 0),
+    %(ErrFlag == -2 *-> (RestOfText = 'invalid penalty', Data = -1, Bar is 1); Bar is 0),
+    %Tik is (Foo + Bar),
+	%(Tik == 0 *-> (Data = MaybeData, RestofText = []); Wee is 0).
 
-getData([H|Text],Data,RestOfText):- H == 
+%getData([H|Text],Data,RestOfText):- H == 
 
-getData(Text,-1,'Error while parsing input file').
+getData(Text,[],'Error while parsing input file',-1).
 	
 	
 take(N, _, Xs):- N =< 0, !, N =:= 0, Xs = [].
--- parserHelper.pl
+++ parserHelper.pl
@@ -1,9 +1,13 @@
 %%changes text from list of ints to list of lists, each list being a line.
 splitTheText([],[]).
+splitTheText(Text,Out):- append(A1,[13,10|A2],Text), %splitting at end-of-line
+    splitTheText(A2,Mid),
+    append([A1],Mid,Some),
+    subtract(Some, [[]], Out).
 splitTheText(Text,Out):- append(A1,[10|A2],Text), %splitting at end-of-line
     splitTheText(A2,Mid),
     append([A1],Mid,Some),
-    subtract(Some, [[],[32]], Out).
+    subtract(Some, [[]], Out).
 splitTheText(Text,[Text]).
 
 
